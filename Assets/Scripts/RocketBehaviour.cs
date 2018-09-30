b07bb51d04e257fe1d83a29e02db29656996876b
vladimir strukelj <vladimir.strukelj@gmail.com>
2018-09-30 23:35:05 +0200

    update

--- Assets/Scripts/RocketBehaviour.cs
+++ Assets/Scripts/RocketBehaviour.cs
@@ -5,32 +5,56 @@ using UnityEngine;
 
 public class RocketBehaviour : MonoBehaviour {
     
-    public Rigidbody rigidbody;
-	// Use this for initialization
-	void Start () {
+    Rigidbody rigidbody;
+    AudioSource boosterSrc;
+    [SerializeField] float rotationFactor;
+    [SerializeField] float thrustFactor;
+    // Use this for initialization
+    void Start () {
         rigidbody = this.GetComponent<Rigidbody>();
-	}
+        boosterSrc = this.gameObject.GetComponent<AudioSource>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        processInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void processInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
-            
-        }
+            rigidbody.AddRelativeForce(Vector3.up * thrustFactor);
+            if (!boosterSrc.isPlaying)
+            {
+                boosterSrc.Play();
+            }
 
+        }
+        else
+        {
+            boosterSrc.Stop();
+        }
+       
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true;
+        var rotationSpeed = rotationFactor * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * 20 * Time.deltaTime);
+            transform.Rotate(Vector3.forward * rotationSpeed);
            
         }else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward * 20 * Time.deltaTime);
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         }
+
+        rigidbody.freezeRotation = false;
     }
+
+
 }
