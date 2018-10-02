9ee33ad1ff5c325b148cb1b1e5003b67ab49da76
vladimir strukelj <vladimir.strukelj@gmail.com>
2018-10-02 22:42:59 +0200

    update

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
 
 namespace Assets.Scripts.Globals
 {
@@ -33,4 +31,13 @@ namespace Assets.Scripts.Globals
         Scene2,
         Scene3
     }
+
+    public enum State
+    {
+        Alive,
+        Dying,
+        Transcending
+    }
+
+    
 }
--- Assets/Scripts/RocketBehaviour.cs
+++ Assets/Scripts/RocketBehaviour.cs
@@ -1,25 +1,45 @@
-﻿using Assets.Scripts.Globals;
+﻿using System;
+using System.Reflection;
+using Assets.Scripts.Globals;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RocketBehaviour : MonoBehaviour {
-    
+public class RocketBehaviour : MonoBehaviour
+{
+
     Rigidbody rigidbody;
     AudioSource boosterSrc;
     [SerializeField] float rotationFactor;
     [SerializeField] float thrustFactor;
+    State state;
+
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        state = State.Alive;
         rigidbody = this.GetComponent<Rigidbody>();
         boosterSrc = this.gameObject.GetComponent<AudioSource>();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        Thrust();
-        Rotate();
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+        else
+        {
+            if (boosterSrc.isPlaying)
+            {
+                boosterSrc.Stop();
+            }
+        }
+
+    }
 
     private void Thrust()
     {
@@ -36,18 +56,20 @@ public class RocketBehaviour : MonoBehaviour {
         {
             boosterSrc.Stop();
         }
-       
+
     }
 
     private void Rotate()
     {
+
         rigidbody.freezeRotation = true;
         var rotationSpeed = rotationFactor * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * rotationSpeed);
-           
-        }else if (Input.GetKey(KeyCode.D))
+
+        }
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward * rotationSpeed);
         }
@@ -57,20 +79,34 @@ public class RocketBehaviour : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
-        string scene = SceneManager.GetActiveScene().name;
-        var newScene = EnumHandler.ConverToEnum<Scenes>(scene) + 1;
+        if(state != State.Alive) { return; }
+
         switch (collision.gameObject.tag)
         {
             case Constants.friendly:
                 print("friendly");
                 break;
             case Constants.landingPad:
-                SceneManager.LoadScene(EnumHandler.ConvertToString(newScene));
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1);
                 break;
             default:
-                SceneManager.LoadScene(EnumHandler.ConvertToString(Scenes.Scene1));
+                state = State.Dying;
+                Invoke("Dead", 1);
                 break;
         }
     }
 
+    private void Dead()
+    {
+        SceneManager.LoadScene(EnumHandler.ConvertToString(Scenes.Scene1));
+    }
+
+    private void LoadNextScene()
+    {
+        var newScene = (EnumHandler.ConverToEnum<Scenes>(SceneManager.GetActiveScene().name) + 1);
+        SceneManager.LoadScene(EnumHandler.ConvertToString(newScene));
+
+
+    }
 }
